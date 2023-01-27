946e67f27b6514353c39a04947f4f397987cc540
Michal Hornický <semtexzv@gmail.com>
2023-01-27 18:42:00 +0100

    fix: Update deps

--- src/command.rs
+++ src/command.rs
@@ -1,17 +1,23 @@
 //! This module contains the commands and all the logic necessary to send them to the display.
-//! Currently is only possible to communicate with the display through SPI protocol. 
+//! Currently is only possible to communicate with the display through SPI protocol.
 use crate::error::Error;
 
-use embedded_hal::spi::blocking::{SpiBus, SpiDevice};
-use embedded_hal::digital::blocking::OutputPin;
+use embedded_hal::digital::OutputPin;
+use embedded_hal::spi::{SpiBus, SpiDevice};
 
 /// Trait that every command must implement. It takes care of sending commands and writing data
 /// to the display. Reading from the display is currently not implemented.
 pub trait Command {
     /// Sends the command and associated argument (if any) to the display.
-    fn send<SPI: SpiDevice>(&self, display: &mut SPI, dc: &mut impl OutputPin) -> Result<(), Error> where SPI::Bus: SpiBus;
+    fn send<SPI: SpiDevice>(&self, display: &mut SPI, dc: &mut impl OutputPin) -> Result<(), Error>
+    where
+        SPI::Bus: SpiBus;
 
-    fn send_command<SPI>(cmd: &[u8], display: &mut SPI, dc: &mut  impl OutputPin) -> Result<(), Error>
+    fn send_command<SPI>(
+        cmd: &[u8],
+        display: &mut SPI,
+        dc: &mut impl OutputPin,
+    ) -> Result<(), Error>
     where
         SPI: SpiDevice,
         SPI::Bus: SpiBus,
@@ -36,10 +42,10 @@ pub trait Command {
 macro_rules! cmd {
     ($t:ty, $l:literal) => {
         impl Command for $t {
-            fn send<SPI>(&self, display: &mut SPI, dc: &mut impl OutputPin) -> Result<(), Error> 
+            fn send<SPI>(&self, display: &mut SPI, dc: &mut impl OutputPin) -> Result<(), Error>
             where
-            SPI: SpiDevice,
-            SPI::Bus: SpiBus,
+                SPI: SpiDevice,
+                SPI::Bus: SpiBus,
             {
                 Self::send_command(&[$l], display, dc)?;
                 Ok(())
@@ -54,8 +60,8 @@ macro_rules! cmd_arg {
         impl Command for $t {
             fn send<SPI>(&self, display: &mut SPI, dc: &mut impl OutputPin) -> Result<(), Error>
             where
-            SPI: SpiDevice,
-            SPI::Bus: SpiBus,
+                SPI: SpiDevice,
+                SPI::Bus: SpiBus,
             {
                 Self::send_command(&[$l], display, dc)?;
                 Self::send_data(&self.0, display, dc)?;
@@ -73,15 +79,23 @@ macro_rules! cmd_arg {
 //     0.. B\[2:0\]
 //   Default: Set A\[8:0\] = 0xc7 and B[2:0\] = 0x0
 pub struct DriverOutputControl(/** 3 bytes command argument. */ pub [u8; 3]);
+cmd_arg!(DriverOutputControl, 0x01);
+
 /// Gate Driving Voltage.
 // 0x03
-pub struct GateDrivingVoltage;
+pub struct GateDrivingVoltage(pub [u8; 1]);
+cmd_arg!(GateDrivingVoltage, 0x03);
+
 /// Source Driving Voltage.
 // 0x04
-pub struct SourceDrivingVoltage;
+pub struct SourceDrivingVoltage(pub [u8; 3]);
+cmd_arg!(SourceDrivingVoltage, 0x04);
+
 /// Initial Code Setting.
 // 0x08
 pub struct InitialCodeSetting;
+cmd!(InitialCodeSetting, 0x08);
+
 /// Booster Soft start control.
 // 0x0c
 //   4 Data bytes:
@@ -89,7 +103,9 @@ pub struct InitialCodeSetting;
 //     1.. B\[6:0\]
 //     1.. C\[6:0\]
 //   Default: A\[7:0\] = 0x8b, B\[7:0\] = 0x9c, C\[7:0\] = 0x96, D\[7:0\] = 0x0f
-pub struct BoosterSoftStartControl(/** 4 bytes command argument. */pub [u8; 4]);
+pub struct BoosterSoftStartControl(/** 4 bytes command argument. */ pub [u8; 4]);
+cmd_arg!(BoosterSoftStartControl, 0x0c);
+
 /// Deep Sleep Mode Control.
 // 0x10
 //   1 Data byte:
@@ -99,6 +115,8 @@ pub struct BoosterSoftStartControl(/** 4 bytes command argument. */pub [u8; 4]);
 //     A\[1:0\] = 0x1: Enter Deep Sleep Mode 1
 //     A\[1:0\] = 0x3: Enter Deep Sleep Mode 2
 pub struct DeepSleepMode(/** 1 byte command argument. */ pub [u8; 1]);
+cmd_arg!(DeepSleepMode, 0x10);
+
 /// Data Entry mode setting.
 // 0x11
 //   1 Data byte: 0.. A\[3:0\]
@@ -108,18 +126,28 @@ pub struct DeepSleepMode(/** 1 byte command argument. */ pub [u8; 1]);
 //     A\[2:0\] = 0x02: (X-, Y+)
 //     A\[2:0\] = 0x03: (X+, Y+)
 pub struct DataEntryModeSetting(/** 1 byte command argument. */ pub [u8; 1]);
+cmd_arg!(DataEntryModeSetting, 0x11);
+
 /// Software reset.
 // 0x12
 pub struct SwReset;
+cmd!(SwReset, 0x12);
+
 /// Temperature Sensor Selection.
 // 0x18
 pub struct TemperatureSensorSelection;
+cmd!(TemperatureSensorSelection, 0x18);
+
 /// Temperature Sensor Write.
 // 0x1a
-pub struct TemperatureSensorWrite(/** 2 bytes command argument. */pub [u8; 2]);
+pub struct TemperatureSensorWrite(/** 2 bytes command argument. */ pub [u8; 2]);
+cmd!(TemperatureSensorWrite, 0x1a);
+
 /// Display update.
 // 0x20
 pub struct MasterActivation;
+cmd!(MasterActivation, 0x20);
+
 /// Display update control 1.
 // 0x21
 //   1 Data byte: 0.. A\[3:0\]
@@ -127,7 +155,9 @@ pub struct MasterActivation;
 //     A\[3:0\] = 0x0: Normal
 //     A\[3:0\] = 0x4: Bypass RAM content as 0
 //     A\[3:0\] = 0x8: Inverse RAM content
-pub struct DisplayUpdateControl1(/** 1 byte command argument. */pub [u8; 1]);
+pub struct DisplayUpdateControl1(/** 1 byte command argument. */ pub [u8; 1]);
+cmd_arg!(DisplayUpdateControl1, 0x21);
+
 /// Display update control 2.
 // 0x22
 //   1 Data byte: A\[7:0\]
@@ -135,53 +165,65 @@ pub struct DisplayUpdateControl1(/** 1 byte command argument. */pub [u8; 1]);
 //     A\[7:0\] = 0xb1: Load LUT with display mode 1
 //     A\[7:0\] = 0xc7: Display with DISPLAY Mode 1
 //     A\[7:0\] = 0xcf: DISPLAY with DISPLAY Mode 2
-pub struct DisplayUpdateControl2(/** 1 byte command argument. */pub [u8; 1]);
+pub struct DisplayUpdateControl2(/** 1 byte command argument. */ pub [u8; 1]);
+cmd_arg!(DisplayUpdateControl2, 0x22);
+
 /// Write RAM.
 // 0x24
 // White pixel: 0x01
 // Black pixel: 0x00
-pub struct WriteRam<'a>(/** Slice command argument. */pub &'a[u8]);
+pub struct WriteRam<'a>(/** Slice command argument. */ pub &'a [u8]);
+cmd_arg!(WriteRam<'_>, 0x24);
+
+// 0x2c
+pub struct WriteVcomRegister(pub [u8; 1]);
+cmd_arg!(WriteVcomRegister, 0x2c);
+
+// 0x32
+pub struct WriteLutRegister<'a>(pub &'a [u8]);
+cmd_arg!(WriteLutRegister<'_>, 0x32);
+
+//0x37
+pub struct WriteOtpSelection<'a>(pub &'a [u8]);
+cmd_arg!(WriteOtpSelection<'_>, 0x37);
+
+pub struct BorderWaveformControl(pub [u8; 1]);
+cmd_arg!(BorderWaveformControl, 0x3C);
+
+// 0x3f
+pub struct WriteLutRegisterEnd(pub [u8; 1]);
+cmd_arg!(WriteLutRegisterEnd, 0x3f);
+
 /// Ram-X Address Position.
 // 0x44
 //   2 Data byte: A\[5:0\], B\[5:0\]
 //   Default Values:
 //     A\[5:0\] = 0x00: Start
 //     B\[5:0\] = 0x15: End
-pub struct SetRamXAddressStartEndPosition(/** 2 bytes command argument. */pub [u8; 2]);
+pub struct SetRamXAddressStartEndPosition(/** 2 bytes command argument. */ pub [u8; 2]);
+cmd_arg!(SetRamXAddressStartEndPosition, 0x44);
+
 /// Ram-Y Address Position.
 // 0x45
 //   4 Data byte: A\[5:0\], B\[5:0\]
 //   Default Values:
 //     A\[8:0\] = \[0x00, 0x00\]: Start
 //     B\[8:0\] = \[0xc7, 0x00\]: End
-pub struct SetRamYAddressStartEndPosition(/** 4 bytes command argument. */pub [u8; 4]);
+pub struct SetRamYAddressStartEndPosition(/** 4 bytes command argument. */ pub [u8; 4]);
+cmd_arg!(SetRamYAddressStartEndPosition, 0x45);
+
 /// Set Ram X-Address Position.
 // 0x4E
-pub struct SetRamXAddressPosition(/** 1 byte command argument. */pub [u8; 1]);
+pub struct SetRamXAddressPosition(/** 1 byte command argument. */ pub [u8; 1]);
+cmd_arg!(SetRamXAddressPosition, 0x4e);
+
 /// Set Ram Y-Address Position
 // 0x4F
-pub struct SetRamYAddressPosition(/** 2 bytes command argument. */pub [u8; 2]);
+pub struct SetRamYAddressPosition(/** 2 bytes command argument. */ pub [u8; 2]);
+cmd_arg!(SetRamYAddressPosition, 0x4f);
+
 /// Empty command.
 // 0x7f
 // Can be used to terminate RAM write/read sequences.
 pub struct Nop;
-
-cmd_arg!(DriverOutputControl, 0x01);
-cmd!(GateDrivingVoltage, 0x03);
-cmd!(SourceDrivingVoltage, 0x04);
-cmd!(InitialCodeSetting, 0x08);
-cmd_arg!(BoosterSoftStartControl, 0x0c);
-cmd_arg!(DeepSleepMode, 0x10);
-cmd_arg!(DataEntryModeSetting, 0x11);
-cmd!(SwReset, 0x12);
-cmd!(TemperatureSensorSelection, 0x18);
-cmd!(TemperatureSensorWrite, 0x1a);
-cmd!(MasterActivation, 0x20);
-cmd_arg!(DisplayUpdateControl1, 0x21);
-cmd_arg!(DisplayUpdateControl2, 0x22);
-cmd_arg!(WriteRam<'_>, 0x24);
-cmd_arg!(SetRamXAddressStartEndPosition, 0x44);
-cmd_arg!(SetRamYAddressStartEndPosition, 0x45);
-cmd_arg!(SetRamXAddressPosition, 0x4e);
-cmd_arg!(SetRamYAddressPosition, 0x4f);
 cmd!(Nop, 0x7f);
--- src/lib.rs
+++ src/lib.rs
@@ -2,41 +2,67 @@
 //! This crate is a `no_std` library that provides an interface compatible with [embedded-hal-1.0.0-alpha.8](https://docs.rs/embedded-hal/1.0.0-alpha.8/embedded_hal/).
 //! It is also designed to be used together with [embedded-graphics](https://docs.rs/embedded-graphics/latest/embedded_graphics/).
 //! It ensures a correct initialization and a consistent state at every moment by enforcing design
-//! constrains at compile time using zero cost abstractions. 
+//! constrains at compile time using zero cost abstractions.
 #![no_std]
 
-mod command;
-pub mod error;
-
-use command::*;
-use error::Error;
-
 use bitvec::prelude::*;
-
 use embedded_graphics::{
     draw_target::DrawTarget, geometry::OriginDimensions, geometry::Size, pixelcolor::BinaryColor,
     Pixel,
 };
+use embedded_hal::delay::DelayUs;
+use embedded_hal::digital::{InputPin, OutputPin};
+use embedded_hal::spi::{SpiBus, SpiDevice};
 
-use embedded_hal::delay::blocking::DelayUs;
-use embedded_hal::digital::blocking::{InputPin, OutputPin};
-use embedded_hal::spi::blocking::{SpiBus, SpiDevice};
+use command::*;
+use error::Error;
+
+mod command;
+pub mod error;
+
+// Original Waveforms from Waveshare for 1in54_V2
+pub(crate) const LUT_FULL_UPDATE: [u8; 159] = [
+    0x80, 0x48, 0x40, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x40, 0x48, 0x80, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x80, 0x48, 0x40, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x40, 0x48, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xA, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x8, 0x1, 0x0, 0x8, 0x1, 0x0,
+    0x2, 0xA, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x22, 0x22, 0x22, 0x22,
+    0x22, 0x22, 0x0, 0x0, 0x0, 0x22, 0x17, 0x41, 0x0, 0x32, 0x20,
+];
+
+pub(crate) const LUT_PARTIAL_UPDATE: [u8; 159] = [
+    0x0, 0x40, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x80, 0x80, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x40, 0x40, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0xF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x22, 0x22, 0x22, 0x22, 0x22,
+    0x22, 0x0, 0x0, 0x0, 0x02, 0x17, 0x41, 0xB0, 0x32, 0x28,
+];
 
 /// Main structure of the library, used to initialize and control de display.
 pub struct GDEH0154D67<SPI, DC, RST, BSY, DLY, S> {
-    interface: SPI,
+    spi: SPI,
     buffer: Buffer,
     dc: DC,
-    reset: RST,
+    rst: RST,
     busy: BSY,
     delay: DLY,
     #[allow(dead_code)]
     state: S,
+
+    lut_partial: bool,
 }
 
 /// Sets the display as initialized, only after calling the method `init()`
 /// the display is considered initialized.
 pub struct Initialized;
+
 /// Sets the display as not initialized. This state occurs when acquiring
 /// a new instance of `GDEH0154D67` or after doing a `reset()` of the display.
 pub struct NotInitialized;
@@ -61,16 +87,17 @@ where
     /// a hardware reset on the device.
     pub fn new(interface: SPI, dc: DC, reset: RST, busy: BSY, delay: DLY) -> Result<Self, Error> {
         let disp = Self {
-            interface,
+            spi: interface,
             buffer: Buffer {
                 pixels: bitarr![u8, Msb0; 0; 40000],
                 flags: bitarr![u8, LocalBits; 0; 40000],
             },
             dc,
-            reset,
+            rst: reset,
             busy,
             delay,
             state: NotInitialized,
+            lut_partial: false,
         };
         disp.reset()
     }
@@ -81,41 +108,41 @@ where
         // 000 -> normal
         // 011 -> upside down
         DriverOutputControl([0xc7, 0x00, 0b000])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
         DataEntryModeSetting([0b011])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
         SetRamXAddressStartEndPosition([0x00, 0x18])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
         SetRamYAddressStartEndPosition([0x00, 0x00, 0xc7, 0x00])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
         TemperatureSensorWrite([0x43, 0x20])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
         DisplayUpdateControl2([0xb1])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
-        MasterActivation
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
+        MasterActivation.send(&mut self.spi, &mut self.dc).unwrap();
         self.busy_block().unwrap();
+
         Ok(GDEH0154D67 {
-            interface: self.interface,
+            spi: self.spi,
             buffer: self.buffer,
             dc: self.dc,
-            reset: self.reset,
+            rst: self.rst,
             busy: self.busy,
             delay: self.delay,
             state: Initialized,
+            lut_partial: self.lut_partial,
         })
     }
 
     /// Releases SPI interface and control pins.
     pub fn release(self) -> Result<(SPI, DC, RST, BSY), Error> {
-        Ok((self.interface, self.dc, self.reset, self.busy))
+        Ok((self.spi, self.dc, self.rst, self.busy))
     }
 }
 
@@ -131,6 +158,10 @@ where
     /// Writes into the displays' RAM only those contents of `Buffer`
     /// that have been modified.
     pub fn partial_update(&mut self) -> Result<(), Error> {
+        if !self.lut_partial {
+            self.set_lut_partial()?;
+        }
+
         for (idx, val) in self
             .buffer
             .flags
@@ -139,58 +170,85 @@ where
             .filter(|(_, val)| val.any())
         {
             let (x, y) = index2address(idx);
-            SetRamXAddressPosition([x])
-                .send(&mut self.interface, &mut self.dc)
-                .unwrap();
-            SetRamYAddressPosition([y, 0x00])
-                .send(&mut self.interface, &mut self.dc)
-                .unwrap();
-
+            SetRamXAddressPosition([x]).send(&mut self.spi, &mut self.dc)?;
+            SetRamYAddressPosition([y, 0x00]).send(&mut self.spi, &mut self.dc)?;
             let raw_data = self.buffer.pixels.as_raw_slice();
-            WriteRam(&[raw_data[idx]])
-                .send(&mut self.interface, &mut self.dc)
-                .unwrap();
+            WriteRam(&[raw_data[idx]]).send(&mut self.spi, &mut self.dc)?;
 
-            self.buffer.pixels[(idx * 8)..((idx + 1) * 8)].store(0);
+            // self.buffer.pixels[(idx * 8)..((idx + 1) * 8)].store(0);
             val.store(0);
         }
 
-        DisplayUpdateControl2([0xc7])
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
-        MasterActivation
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
-        self.busy_block().unwrap();
+        DisplayUpdateControl2([0xC7]).send(&mut self.spi, &mut self.dc)?;
+        MasterActivation.send(&mut self.spi, &mut self.dc)?;
+        self.busy_block()?;
         Ok(())
     }
 
     /// Writes all the contents of the `Buffer` into the RAM of the display and
     /// performs a display update.
     pub fn full_update(&mut self) -> Result<(), Error> {
-        SetRamXAddressPosition([0x00])
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
-        SetRamYAddressPosition([0x00, 0x00])
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
-        WriteRam(self.buffer.pixels.as_raw_slice())
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
-        DisplayUpdateControl2([0xc7])
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
-        MasterActivation
-            .send(&mut self.interface, &mut self.dc)
-            .unwrap();
+        if self.lut_partial {
+            self.set_lut_full()?;
+        }
+        SetRamXAddressPosition([0x00]).send(&mut self.spi, &mut self.dc)?;
+        SetRamYAddressPosition([0x00, 0x00]).send(&mut self.spi, &mut self.dc)?;
+        WriteRam(self.buffer.pixels.as_raw_slice()).send(&mut self.spi, &mut self.dc)?;
+        DisplayUpdateControl2([0xCF]).send(&mut self.spi, &mut self.dc)?;
+        MasterActivation.send(&mut self.spi, &mut self.dc)?;
         self.busy_block().unwrap();
         Ok(())
     }
 
+    pub fn write_raw_lut(&mut self, lut: &[u8]) -> Result<(), Error> {
+        WriteLutRegister(&lut).send(&mut self.spi, &mut self.dc)
+    }
+
+    pub fn set_lut(&mut self, lut: &[u8]) -> Result<(), Error> {
+        self.busy_block()?;
+
+        WriteLutRegister(&lut[0..153]).send(&mut self.spi, &mut self.dc)?;
+        WriteLutRegisterEnd([lut[153]]).send(&mut self.spi, &mut self.dc)?;
+        self.busy_block()?;
+
+        GateDrivingVoltage([lut[154]]).send(&mut self.spi, &mut self.dc)?;
+        SourceDrivingVoltage([lut[155], lut[156], lut[157]]).send(&mut self.spi, &mut self.dc)?;
+        WriteVcomRegister([lut[158]]).send(&mut self.spi, &mut self.dc)?;
+
+        Ok(())
+    }
+
+    pub fn set_lut_full(&mut self) -> Result<(), Error> {
+        self.set_lut(&LUT_FULL_UPDATE)?;
+        self.lut_partial = false;
+        Ok(())
+    }
+
+    pub fn set_lut_partial(&mut self) -> Result<(), Error> {
+        self.set_lut(&LUT_PARTIAL_UPDATE)?;
+
+        WriteOtpSelection(&[0x0, 0x0, 0x0, 0x0, 0x0, 0x40, 0x0, 0x0, 0x0, 0x0])
+            .send(&mut self.spi, &mut self.dc)
+            .unwrap();
+
+        BorderWaveformControl([0x80])
+            .send(&mut self.spi, &mut self.dc)
+            .unwrap();
+
+        DisplayUpdateControl2([0xc0])
+            .send(&mut self.spi, &mut self.dc)
+            .unwrap();
+
+        MasterActivation.send(&mut self.spi, &mut self.dc).unwrap();
+
+        Nop.send(&mut self.spi, &mut self.dc).unwrap();
+        self.lut_partial = true;
+        Ok(())
+    }
     /// Releases SPI interface and control pins.
     pub fn release(self) -> Result<(SPI, DC, RST, BSY), Error> {
         let display = self.turn_off().unwrap();
-        Ok((display.interface, display.dc, display.reset, display.busy))
+        Ok((display.spi, display.dc, display.rst, display.busy))
     }
 }
 
@@ -204,37 +262,42 @@ where
     DLY: DelayUs,
 {
     /// Sends the display into deep sleep mode.
-    pub fn turn_off(mut self) -> Result<GDEH0154D67<SPI, DC, RST, BSY, DLY, NotInitialized>, Error> {
+    pub fn turn_off(
+        mut self,
+    ) -> Result<GDEH0154D67<SPI, DC, RST, BSY, DLY, NotInitialized>, Error> {
         DeepSleepMode([0x1])
-            .send(&mut self.interface, &mut self.dc)
+            .send(&mut self.spi, &mut self.dc)
             .unwrap();
+
         Ok(GDEH0154D67 {
-            interface: self.interface,
+            spi: self.spi,
             buffer: self.buffer,
             dc: self.dc,
-            reset: self.reset,
+            rst: self.rst,
             busy: self.busy,
             delay: self.delay,
             state: NotInitialized,
+            lut_partial: self.lut_partial,
         })
     }
 
     /// Performs a hardware reset of the display.
     fn reset(mut self) -> Result<GDEH0154D67<SPI, DC, RST, BSY, DLY, NotInitialized>, Error> {
-        self.reset.set_low().unwrap();
+        self.rst.set_low().unwrap();
         DelayUs::delay_ms(&mut self.delay, 10).unwrap();
-        self.reset.set_high().unwrap();
+        self.rst.set_high().unwrap();
         DelayUs::delay_ms(&mut self.delay, 10).unwrap();
-        SwReset.send(&mut self.interface, &mut self.dc).unwrap();
+        SwReset.send(&mut self.spi, &mut self.dc).unwrap();
         self.busy_block().unwrap();
         Ok(GDEH0154D67 {
-            interface: self.interface,
+            spi: self.spi,
             buffer: self.buffer,
             dc: self.dc,
-            reset: self.reset,
+            rst: self.rst,
             busy: self.busy,
             delay: self.delay,
             state: NotInitialized,
+            lut_partial: false,
         })
     }
 
